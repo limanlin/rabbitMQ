bcae59eac5eba2b2a33c17ad1915695870f4a856
limanlin <435236478@qq.com>
2020-10-10 16:46:22 +0800

    rabbitmq

---
+++ rabbitmq/src/main/java/m1_simple/Consumer.java
@@ -0,0 +1,45 @@
+package m1_simple;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Consumer {
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory f = new ConnectionFactory();
+        f.setHost("192.168.64.140");
+        f.setPort(5672);
+        f.setUsername("admin");
+        f.setPassword("admin");
+        Channel cc = f.newConnection().createChannel();
+        //定义队列
+        //告诉服务器想使用的队列
+        //服务器检查队列如果不存在，则回新建队列
+        cc.queueDeclare("helloworld", false, false, false, null);
+        /*生产者和消费者都创建了一个队列的原因是？
+        保证存在，两边都创建则不会因为消费者比生产者早启动而导致找不到队列
+        * */
+        //处理消息的回调对象
+        DeliverCallback deliverCallback = new DeliverCallback() {
+            @Override
+            public void handle(String s, Delivery delivery) throws IOException {
+
+            }
+        };
+        // 取消接收信息的回调对象
+        CancelCallback cancelCallback = new CancelCallback() {
+            @Override
+            public void handle(String s) throws IOException {
+
+            }
+        };
+
+        //3,消费数据
+        cc.basicConsume("helloworld",true,
+                //处理消息的回调对象
+                deliverCallback,
+                //取消接受信息回调对象
+                cancelCallback);
+    }
+}
---
+++ rabbitmq/src/main/java/m1_simple/Producer.java
@@ -0,0 +1,34 @@
+package m1_simple;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Producer {
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory f = new ConnectionFactory();
+        f.setHost("192.168.64.140");//外网主机域名
+        f.setPort(5672);
+        f.setUsername("admin");
+        f.setPassword("admin");
+        //如果连接外网的服务器，需要设置指定虚拟主机
+        f.setVirtualHost("/lml");
+        //创建通道
+        Connection cc = f.newConnection();
+        Channel c = cc.createChannel();
+        //定义队列，指定某个队列
+        c.queueDeclare("helloworld",
+                false,//是否是持久队列
+                false,//是否是排他队列（消费者独占的队列）
+                false,//是否自动删除（没有消费者时，自动删除）
+                null);//其他属性
+
+        //发送消息
+        c.basicPublish("","helloworld",null,//其他参数
+                "helloworld".getBytes());
+        System.out.println("消息已发送");
+        c.close();
+        cc.close();
+
+    }
+}
